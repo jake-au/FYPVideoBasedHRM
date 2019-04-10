4be359f4fda0a28b1157a6162c7a5a7f569e5dd3
Jiakai Ren <jake.ren@icloud.com>
2019-04-10 21:12:34 +1000

    Update heart_rate_monitor.m

--- heart_rate_monitor.m
+++ heart_rate_monitor.m
@@ -15,8 +15,10 @@ clc
 % ------------------------- %
 
 % - Modify these parameters: - %
-vName = ('2.mp4');
+vName = ('1.mp4');
 Cropped = 1; % 1 for cropped video, 0 for uncropped video.
+find_peaks_start = 3; % on which second should the peak findings start.
+secs_per_measure = 5; % number of seconds used to read one HR out.
 % ----------END--------------- %
 
 vForInfo = VideoReader(vName);
@@ -24,6 +26,7 @@ vNumberOfFrames = get(vForInfo, 'NumberOfFrames');
 vHeight = get(vForInfo, 'Height');
 vWidth = get(vForInfo, 'Width');
 vFrameRate = get(vForInfo, 'FrameRate');
+vDuration = get(vForInfo, 'Duration');
 
 % INITIALISE RGB MATRICES
 r = zeros(vHeight, vWidth, vNumberOfFrames, 'uint8');
@@ -80,3 +83,43 @@ hold off
 figure(2)
 plot(gMeanAllFramesNorm(:,1)/vFrameRate,gMeanAllFramesNorm(:,2));
 xlabel('Time (s)'); ylabel('Green Channel Intensity');
+
+% --------- %
+%| FILTERS |%
+% --------- %
+
+% FOURIER TRANSFORM
+G_mags = abs(fft(gMeanAllFramesNorm(:,2)));
+figure(3)
+stem([0:2/(length(G_mags) -1):2], G_mags);
+xlabel('Normalised Frequency (\pi rad/frame)');
+ylabel('Magnitude');
+
+% FILTER
+[b a] = butter(2, [0.12], 'low') % Butterworth lowpass filter
+H = freqz(b,a, vFrameRate); % Frequency response
+hold on
+plot([0:1/(vFrameRate -1):1], abs(H), 'r');
+hold off
+
+% APPLY FILTER
+g_filtered = filter(b,a,gMeanAllFramesNorm(:,2));
+
+figure(2)
+plot(gMeanAllFramesNorm(:,1)/vFrameRate,gMeanAllFramesNorm(:,2));
+xlabel('Time (s)'); ylabel('Green Channel Intensity');
+hold on
+plot(gMeanAllFramesNorm(:,1)/vFrameRate, g_filtered, 'r')
+
+[PKS, LOCS] = findpeaks(g_filtered(150:vNumberOfFrames));
+HR = length(PKS)/((vNumberOfFrames-150)/vFrameRate) * 60
+plot((LOCS+150)/vFrameRate, PKS, 'g*');
+hold off
+
+% g_filtered_1 = diff(g_filtered);
+% PKS_2 = findpeaks(g_filtered_1);
+
+% secs_measured = floor(vDuration);
+% 
+% for m = find_peaks_start + secs_per_measure -1:secs_measured - secs_per_measure +1
+%     
