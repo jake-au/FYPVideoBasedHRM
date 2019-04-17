5bea29fad9b26b95a5837197cf31beee89d7228b
Jiakai Ren <jake.ren@icloud.com>
2019-04-17 15:11:09 +1000

    Update heart_rate_monitor.m

--- heart_rate_monitor.m
+++ heart_rate_monitor.m
@@ -15,9 +15,9 @@ clc
 % ------------------------- %
 
 % - Modify these parameters: - %
-vName = ('1.mp4');
+vName = ('test6.mp4');
 Cropped = 1; % 1 for cropped video, 0 for uncropped video.
-find_peaks_start = 3; % on which second should the peak findings start.
+find_peaks_start = 5; % on which second should the peak findings start.
 secs_per_measure = 5; % number of seconds used to read one HR out.
 % ----------END--------------- %
 
@@ -69,9 +69,9 @@ end
 gMeanAllFramesNorm = gMeanAllFrames;
 gMeanAllFramesNorm(:,2) = gMeanAllFrames(:,2)/norm(gMeanAllFrames(:,2));
 
-% -------- %
-%| GRAPHS |%
-% -------- %
+% ------------ %
+%| FRAME SHOW |%
+% ------------ %
 
 figure(1)
 imshow(g(:,:,1));
@@ -80,40 +80,58 @@ figure(1)
 rectangle('Position', [290, 100, 40, 40], 'LineWidth', 1, 'LineStyle', '-')
 hold off
 
-figure(2)
-plot(gMeanAllFramesNorm(:,1)/vFrameRate,gMeanAllFramesNorm(:,2));
-xlabel('Time (s)'); ylabel('Green Channel Intensity');
-
 % --------- %
 %| FILTERS |%
 % --------- %
 
 % FOURIER TRANSFORM
 G_mags = abs(fft(gMeanAllFramesNorm(:,2)));
-figure(3)
+figure(2)
 stem([0:2/(length(G_mags) -1):2], G_mags);
 xlabel('Normalised Frequency (\pi rad/frame)');
 ylabel('Magnitude');
 
-% FILTER
-[b a] = butter(2, [0.12], 'low') % Butterworth lowpass filter
-H = freqz(b,a, vFrameRate); % Frequency response
+% FILTER - ORIGINAL SINGLE FILTER
+% [b a] = butter(5, [0.111], 'low') % Butterworth lowpass filter
+% H = freqz(b,a, vFrameRate); % Frequency response
+% hold on
+% plot([0:1/(vFrameRate -1):1], abs(H), 'r');
+% hold off
+
+% NEW CASCADED FILTER
+% FILTER 1 - LOW PASS
+[b1 a1] = butter(5, [0.111], 'low') % Butterworth lowpass filter
+H1 = freqz(b1,a1, vFrameRate); % Frequency response
 hold on
-plot([0:1/(vFrameRate -1):1], abs(H), 'r');
+plot(0:1/(vFrameRate -1):1, abs(H1), 'r');
+
+% FILTER 2 - NOTCH
+wo = 0.0001;  bw = 0.04;
+[b2,a2] = iirnotch(wo,bw);
+H2 = freqz(b2,a2, vFrameRate);
+plot(0:1/(vFrameRate -1):1, abs(H2), 'b');
 hold off
 
-% APPLY FILTER
-g_filtered = filter(b,a,gMeanAllFramesNorm(:,2));
+% FILTER CASCADING
+H1=dfilt.df2t(b1,a1);
+H2=dfilt.df2t(b2,a2);
+Hcas=dfilt.cascade(H1,H2)  
+info(Hcas.Stage(1))
+hfvt= fvtool(Hcas,'Color','white');
 
-figure(2)
-plot(gMeanAllFramesNorm(:,1)/vFrameRate,gMeanAllFramesNorm(:,2));
-xlabel('Time (s)'); ylabel('Green Channel Intensity');
+% APPLY FILTER
+g_filtered = filter(Hcas,gMeanAllFramesNorm(:,2));
+
+figure(4)
+plot(gMeanAllFramesNorm(:,1)/vFrameRate,gMeanAllFramesNorm(:,2)); % without subtracting the green channel mean
+plot(gMeanAllFramesNorm(:,1)/vFrameRate,gMeanAllFramesNorm(:,2)-mean(gMeanAllFramesNorm(:,2))); % minus the green channel mean so comparason easier
+xlabel('Time (s)'); ylabel('Green Channel Intensity - Intensity Mean');
 hold on
 plot(gMeanAllFramesNorm(:,1)/vFrameRate, g_filtered, 'r')
 
-[PKS, LOCS] = findpeaks(g_filtered(150:vNumberOfFrames));
-HR = length(PKS)/((vNumberOfFrames-150)/vFrameRate) * 60
-plot((LOCS+150)/vFrameRate, PKS, 'g*');
+[PKS, LOCS] = findpeaks(g_filtered(find_peaks_start*vFrameRate:vNumberOfFrames));
+HR = length(PKS)/((vNumberOfFrames-find_peaks_start*vFrameRate)/vFrameRate) * 60
+plot((LOCS+find_peaks_start*vFrameRate)/vFrameRate, PKS, 'g*');
 hold off
 
 % g_filtered_1 = diff(g_filtered);
